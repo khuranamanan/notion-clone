3f29e4df51cafbc619d19b48a6ee8caaf27ef625
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-12-10 18:25:19 +0530

    feat: Trash Documents Banner

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import Title from "./Title";
+import Banner from "./Banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -32,18 +33,21 @@ function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
   }
 
   return (
-    <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
-      {isCollapsed && (
-        <MenuIcon
-          role="button"
-          onClick={onResetWidth}
-          className="h-6 w-6 text-muted-foreground"
-        />
-      )}
-      <div className="flex items-center justify-between w-full">
-        <Title initialData={document} />
-      </div>
-    </nav>
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
+    </>
   );
 }
 
